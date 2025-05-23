a81f54df5b52dc1f7c7b3908cdfc63e929513676
Majid <majidsa520@outlook.com>
2025-05-23 12:52:20 +0330

    organize layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import Settings from "./pages/Settings";
 import Users from "./pages/Users";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
 	return (
@@ -15,13 +16,15 @@ function App() {
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
-					<Route index element={<Navigate replace to="/dashboard" />} />
-					<Route path="/account" element={<Account />} />
-					<Route path="/bookings" element={<Bookings />} />
-					<Route path="/cabins" element={<Cabins />} />
-					<Route path="/dashboard" element={<Dashboard />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/settings" element={<Settings />} />
+					<Route element={<AppLayout />}>
+						<Route index element={<Navigate replace to="/dashboard" />} />
+						<Route path="/account" element={<Account />} />
+						<Route path="/bookings" element={<Bookings />} />
+						<Route path="/cabins" element={<Cabins />} />
+						<Route path="/dashboard" element={<Dashboard />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/settings" element={<Settings />} />
+					</Route>
 					<Route path="/users" element={<Users />} />
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>

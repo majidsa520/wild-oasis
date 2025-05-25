2ccf2c8155655100439ebf2f6a777e05292e4028
Majid <majidsa520@outlook.com>
2025-05-25 11:50:03 +0330

    forgot to add supabase stuff :D 1- install reactquery with npm command: npm i @tanstack/react-query@4 or npm i @tanstack/react-query@latest 2- setup react queryClient and Provider 3- install reactquery devtools: npm i @tanstak/react-query-devtools@latest 4- add devtools to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import GlobalStyles from "./styles/GlobalStyles";
 import Account from "./pages/Account";
 import Bookings from "./pages/Bookings";
@@ -11,8 +13,17 @@ import Users from "./pages/Users";
 import AppLayout from "./ui/AppLayout";
 
 function App() {
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				staleTime: 60 * 1000,
+			},
+		},
+	});
+
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen="false" />
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
@@ -29,7 +40,7 @@ function App() {
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
-		</>
+		</QueryClientProvider>
 	);
 }
 
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,23 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
-  );
+	useEffect(() => {
+		//const cabins = await getCabins()
+		//console.log(cabins);
+	}, []);
+	return (
+		<Row type="horizontal">
+			<Heading as="h1">All cabins</Heading>
+			<p>TEST</p>
+			<img
+				src="https://ohxvrgskowfwbbgwjlvo.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg"
+				alt="cabin in the woods photo"
+			/>
+		</Row>
+	);
 }
 
 export default Cabins;

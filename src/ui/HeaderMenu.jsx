b29f75e7c3f9b5ed49f1141f7383ce44c3e3cc9d
Majid <majidsa520@outlook.com>
2025-07-26 19:12:43 +0330

    add some things to main header

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import Logout from "../features/authentication/Logout";
+import ButtonIcon from "./ButtonIcon";
+import { HiOutlineUser } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+
+const StyledMenu = styled.ul`
+	display: flex;
+	gap: 0.4rem;
+`;
+export default function HeaderMenu() {
+	const navigate = useNavigate();
+	return (
+		<StyledMenu>
+			<li>
+				<ButtonIcon onClick={() => navigate("/account")}>
+					<HiOutlineUser />
+				</ButtonIcon>
+			</li>
+			<li>
+				<Logout />
+			</li>
+		</StyledMenu>
+	);
+}

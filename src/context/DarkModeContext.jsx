39a66fc40eceb6261225ce948a5d68817d408873
Majid <majidsa520@outlook.com>
2025-07-31 16:08:15 +0330

    change default dark mode with window.matchMedia('(prefers-color-scheme: dark)')

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -4,7 +4,7 @@ import { useLocalStorageState } from "../hooks/useLocalStorageState";
 const DarkModeContext = createContext();
 function DarkModeProvider({ children }) {
 	const [isDarkMode, setIsDarkMode] = useLocalStorageState(
-		false,
+		window.matchMedia("(prefers-color-scheme: dark)"),
 		"isDarkMode"
 	);
 	function toggleDarkMode() {

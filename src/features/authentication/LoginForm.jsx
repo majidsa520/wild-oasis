12ec81d2abc99aac3b4dd7fe3951b6a0dad74fa7
Majid <majidsa520@outlook.com>
2025-10-25 23:13:19 +0330

    fixed some bugs

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("majidsa520@outlook.com");
+	const [password, setPassword] = useState("Lu520WPh");
 	const { loginUser, isLoggingin } = useLogin();
 	function handleSubmit(e) {
 		e.preventDefault();
--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -31,10 +31,10 @@ export default function TodayItem({ activity }) {
 			{status === "unconfirmed" && <Tag type="green">Arriving</Tag>}
 			{status === "checked-in" && <Tag type="blue">Departing</Tag>}
 			<Flag
-				src={guests.countryFlag}
-				alt={`Flag of country ${guests.country}`}
+				src={guests?.countryFlag}
+				alt={`Flag of country ${guests?.country}`}
 			/>
-			<Guest>{guests.fullName}</Guest>
+			<Guest>{guests?.fullName}</Guest>
 			<div>{numNights} nights</div>
 			{status === "unconfirmed" && (
 				<Button
